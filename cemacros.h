e7419f604ae2b1dd7586d769669412c7f67e0743
coldnew <coldnew.tw@gmail.com>
2009-12-26 04:20:59 +0800

    update

--- cemacros.h
+++ cemacros.h
@@ -1,6 +1,6 @@
 /* CELIB - cldnew's C libraries designed for CEMACS
  *
- * Copyright (C) 2009  Yen-Chin, Lee. All Rights Reserved. 
+ * Copyright (C) 2009-2010  Yen-Chin, Lee. All Rights Reserved. 
  *
  * This file is part of CELIB.
  *
