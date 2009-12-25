c557d3611e0823f654015f299b86a5800d5ab166
coldnew <coldnew.tw@gmail.com>
2009-12-25 19:40:44 +0800

    updata cmake

--- cestring.c
+++ cestring.c
@@ -18,8 +18,8 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
-//#include "celib.h"
-#include "cestring.h"
+#include "celib.h"
+//#include "cestring.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
