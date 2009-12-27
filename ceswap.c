0911bde0db6079d608d60627fb931765a1539243
coldnew <coldnew.tw@gmail.com>
2009-12-27 13:30:57 +0800

    update

--- ceswap.c
+++ ceswap.c
@@ -21,10 +21,11 @@
  *
  */
 
-#include "cetypes.h"
-#include "ceswap.h"
+//#include "cetypes.h"
+//#include "ceswap.h"
 
 #include <stdio.h>
+#include "celib.h"
 
 #define CE_SWAP_CORE  tmp = *p1; *p1 = *p2; *p2 = tmp    
 
